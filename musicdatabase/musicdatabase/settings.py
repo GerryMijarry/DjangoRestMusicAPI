63b788adba32e6d5e52326563ff3f653ebfc2736
Gerard Mijares <gerardmijares39@gmail.com>
2021-10-05 21:37:02 +0400

    deleted appropriate data from settings.py

--- musicdatabase/musicdatabase/settings.py
+++ musicdatabase/musicdatabase/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3l+=&06p=&p9(=0+!tauzl461=a@@brq*s)f6cnuc9v)_1lhts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,13 +72,6 @@ WSGI_APPLICATION = 'musicdatabase.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
